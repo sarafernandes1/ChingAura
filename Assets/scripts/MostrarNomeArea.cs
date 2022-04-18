c1127aab6f535c1b7d362abb5a3ad187a5973c93
sarafernandes1 <82063336+sarafernandes1@users.noreply.github.com>
2022-04-18 19:24:33 +0100

    aparecer nome do local no ecrã

---
+++ Assets/scripts/MostrarNomeArea.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostrarNomeArea : MonoBehaviour
+{
+    public Text texto_nome;
+    public bool isInArea, a=true;
+    public string nome_do_sitio;
+    public RawImage imagem;
+    public float timer = 1000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInArea && a)
+        {
+             texto_nome.enabled = true;
+             imagem.enabled = true;
+             texto_nome.text = transform.name;
+            timer-=0.2f;
+            
+            if (timer < 0 || !isInArea)
+            {
+                texto_nome.enabled = false;
+                imagem.enabled = false;
+                a = false;
+                timer = 100;
+            }
+
+        }
+        if (!isInArea && timer > 0.0f)
+        {
+            texto_nome.enabled = false;
+            imagem.enabled = false;
+            timer = 100;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) //qd entra no trigger
+    {
+        if (other.tag == "Player")
+        {
+            isInArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) //qd sai do trigger
+    {
+        if (other.tag == "Player")
+        {
+            isInArea = false;
+            a = true;
+        }
+    }
+}
